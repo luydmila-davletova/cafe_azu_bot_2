5130c8617aa49fb518af73a2cb7f708f3b3d2d51
Certelen <dst98@yandex.ru>
2023-11-17 05:58:40 +0500

    user validation

--- azu_bot_django/admin_users/admin.py
+++ azu_bot_django/admin_users/admin.py
@@ -5,13 +5,13 @@ from admin_users.models import CustomUser
 
 ADD_CUSTOM_FIELDS_IN_USER_FORM = ((
     "Работает в кафе",
-    {"fields": ("cafe", "is_cafe_admin",)}
+    {"fields": ("cafe",)}
 ),)
 
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     list_display = ("username", "first_name",
-                    "last_name", "cafe", "is_cafe_admin")
+                    "last_name", "cafe")
     UserAdmin.fieldsets += ADD_CUSTOM_FIELDS_IN_USER_FORM
     UserAdmin.add_fieldsets += ADD_CUSTOM_FIELDS_IN_USER_FORM
--- azu_bot_django/admin_users/models.py
+++ azu_bot_django/admin_users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.core.exceptions import ValidationError
+
 
 from cafe.models import Cafe
 from reservation.models import OrderSets, Reservation
@@ -15,7 +17,6 @@ class CustomUser(AbstractUser):
         null=True,
         help_text="Для сотрудников кафе обязателен выбор к привязанному кафе",
     )
-    is_cafe_admin = models.BooleanField(default=False)
 
     class Meta:
         verbose_name = 'Пользователь'
@@ -27,26 +28,28 @@ class CustomUser(AbstractUser):
 
     def clean(self):
         """Если пользователь админ - может заходить в админку"""
-        if self.is_cafe_admin:
-            self.is_staff = True
+        if not self.cafe:
+            raise ValidationError(
+                "Выберите кафе к которому приписан администратор"
+            )
+        self.is_staff = True
 
     def save(self, force_insert=False, force_update=False, *args, **kwargs):
         """
         Автоматическое добавление прав доступа при создании
         """
         super().save(force_insert, force_update, *args, **kwargs)
-        if self.is_cafe_admin:
-            cafe_admin_group, created = Group.objects.get_or_create(
-                name='Cafe Admin'
-            )
-            self.groups.add(cafe_admin_group)
-            reservations_permissions = Permission.objects.filter(
-                content_type=ContentType.objects.get_for_model(Reservation)
-            )
-            order_sets_permissions = Permission.objects.filter(
-                content_type=ContentType.objects.get_for_model(OrderSets)
-            )
-            self.user_permissions.add(
-                *reservations_permissions,
-                *order_sets_permissions
-            )
+        cafe_admin_group, created = Group.objects.get_or_create(
+            name='Cafe Admin'
+        )
+        self.groups.add(cafe_admin_group)
+        reservations_permissions = Permission.objects.filter(
+            content_type=ContentType.objects.get_for_model(Reservation)
+        )
+        order_sets_permissions = Permission.objects.filter(
+            content_type=ContentType.objects.get_for_model(OrderSets)
+        )
+        self.user_permissions.add(
+            *reservations_permissions,
+            *order_sets_permissions
+        )
