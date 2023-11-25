c331b95737e9e9755753c53cf79c401131b5db06
Владимир Захаров <zakharov_v_s@hotmail.com>
2023-11-26 06:05:06 +0700

    Async Django. Websockets. Uvicorn. Asgi.

--- azu_bot_django/tables/admin.py
+++ azu_bot_django/tables/admin.py
@@ -9,7 +9,6 @@ from tables.models import Table
 class TableAdmin(admin.ModelAdmin):
     list_display = ('name', 'cafe', 'quantity')
 
-    @sync_to_async
     def get_form(self, request, obj=None, **kwargs):
         """
         При создании нового объекта другие экземпляры моделей
@@ -22,7 +21,6 @@ class TableAdmin(admin.ModelAdmin):
             cafe=request.user.cafe.id)
         return form
 
-    @sync_to_async
     def get_queryset(self, request):
         """
         Персонал получит информацию о столах в его кафе,
