ca3516ca9e425caade9a374f4a656f362791c06c
madina <madi92@mail.ru>
2023-11-25 14:21:48 +0300

    тест уведомлений в боте

--- azu_bot_django/reservation/admin.py
+++ azu_bot_django/reservation/admin.py
@@ -1,93 +1,59 @@
-from django import forms
 from django.contrib import admin
-from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse, path
 from django.utils.html import format_html
 from django.utils.http import urlencode
-
-from cafe.models import Cafe
-from reservation.models import OrderSets, Reservation
-from reservation.validation import (tables_available, tables_in_cafe,
-                                    tables_in_cafe_in_date)
-from tables.models import Table
-
-from .models import OrderSets, Reservation
+from reservation.models import Reservation, OrderSets
 
 
-class OrderSetsInline(admin.TabularInline):
-    model = OrderSets
+class ReservationAdminInline(admin.TabularInline):
+    model = Reservation.sets.through
     extra = 1
 
 
-class ReservationForm(forms.ModelForm):
-
-    def clean(self):
-        """
-        Проверка полей
-        """
-        super().clean()
-        if self.is_valid():
-            tables_in_cafe(self)
-            tables_in_cafe_in_date(self)
-            tables_available(self)
-        return self.cleaned_data
+@admin.register(OrderSets)
+class OrderSetsAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(Reservation)
 class ReservationAdmin(admin.ModelAdmin):
-    readonly_fields = ('id',)
-    list_display = ('cafe', 'view_tables', 'view_order_sets',
-                    'name', 'number', 'date')
-    list_filter = ('date', 'status')
-    inlines = [OrderSetsInline]
-    form = ReservationForm
-
-    def get_form(self, request, obj=None, **kwargs):
-        """
-        Получение экземляров других моделей, приписанных пользователю
-        при создании брони
-        """
-        form = super(ReservationAdmin, self).get_form(request, obj, **kwargs)
-        if request.user.is_superuser:
-            return form
-        form.base_fields['cafe'].queryset = Cafe.objects.filter(
-            pk=request.user.cafe.id)
-        form.base_fields['table'].queryset = Table.objects.filter(
-            cafe=request.user.cafe.id)
-        return form
+    list_display = ("cafe", "view_tables", "view_order_sets", "name", "number", "date")
+    list_filter = ("date", "cafe")
+    inlines = [ReservationAdminInline]
 
     def get_queryset(self, request):
         queryset = super().get_queryset(request)
         if not request.user.is_superuser:
-            queryset = queryset.filter(
-                cafe=request.user.cafe
-            )
+            queryset = queryset.filter(cafe=request.user.profile.cafe)
         return queryset
 
+    def save_model(self, request, obj, form, change):
+        if not request.user.is_superuser:
+            obj.cafe = request.user.profile.cafe
+        super().save_model(request, obj, form, change)
+
     def view_tables(self, obj):
-        """
-        Отображение количества забронированых столов
-        и их вместительности на панели броней
-        """
         count = obj.table.count()
-        quantity = ''
         if count == 1:
             short_description = ' Стол '
         elif 1 < count < 5:
             short_description = ' Стола '
         else:
-            short_description = ' Столов '
-        for table in obj.table.all():
-            if quantity:
-                quantity += ', '
-            quantity += str(table.quantity)
-        return str(count) + short_description + 'на ' + quantity + ' человек'
+            short_description = 'Столов'
+
+        url = reverse("admin:tables_table_changelist")
+        if count > 0:
+            url += f"?{urlencode({'id__in': obj.table.values_list('id', flat=True)})}"
+
+        return format_html(
+            '<a href="{}">{} {}</a>', url, count, short_description
+        )
+
     view_tables.short_description = 'Столов'
 
     def view_order_sets(self, obj):
-        """
-        Отображение количества заказанных сетов
-        и переход на панель с этими заказами
-        """
         count = obj.sets.count()
         if count == 1:
             short_description = 'Заказ'
@@ -95,36 +61,45 @@ class ReservationAdmin(admin.ModelAdmin):
             short_description = 'Заказа'
         else:
             short_description = 'Заказов'
-        url = (
-            reverse('admin:reservation_ordersets_changelist')
-            + '?'
-            + urlencode({'reservations__id': f'{obj.id}'})
-        )
+
+        url = reverse("admin:reservation_order_sets_changelist", args=[obj.id])
+        if count > 0:
+            url += f"?{urlencode({'reservation__id__exact': f'{obj.id}'})}"
+
         return format_html(
             '<a href="{}">{} {}</a>', url, count, short_description
         )
-    view_order_sets.short_description = 'Сетов'
 
+    view_order_sets.short_description = "Сетов"
 
-@admin.register(OrderSets)
-class OrderSetsAdmin(admin.ModelAdmin):
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path(
+                '<path:reservation_id>/order_sets/',
+                self.admin_site.admin_view(self.order_sets_changelist),
+                name='reservation_order_sets_changelist',
+            ),
+        ]
+        return custom_urls + urls
 
-    def get_form(self, request, obj=None, **kwargs):
-        """
-        Получение экземляров других моделей, приписанных пользователю
-        при создании брони
-        """
-        form = super(OrderSetsAdmin, self).get_form(request, obj, **kwargs)
-        if request.user.is_superuser:
-            return form
-        form.base_fields['reservation'].queryset = Reservation.objects.filter(
-            cafe=request.user.cafe.id)
-        return form
+    def order_sets_changelist(self, request, reservation_id):
+        reservation = get_object_or_404(Reservation, pk=reservation_id)
 
-    def get_queryset(self, request):
-        queryset = super().get_queryset(request)
-        if not request.user.is_superuser:
-            queryset = queryset.filter(
-                reservation__cafe__id=request.user.cafe.id
-            )
-        return queryset
+        if request.method == 'POST':
+            set_id = request.POST.get('set_id')
+            quantity = request.POST.get('quantity')
+            OrderSets.objects.create(reservation=reservation, set_id=set_id, quantity=quantity)
+            url = reverse("admin:reservation_order_sets_changelist", args=[reservation_id])
+            return HttpResponseRedirect(url)
+
+        context = {
+            'reservation': reservation,
+            'order_sets': reservation.order_sets.all(),
+        }
+        return render(request, 'admin/reservation/ordersets/change_list.html', context)
+
+    def delete_reservation(self, request, reservation_id):
+        reservation = get_object_or_404(Reservation, pk=reservation_id)
+        reservation.delete()
+        return HttpResponseRedirect(reverse("admin:reservation_reservation_changelist"))
--- azu_bot_django/reservation/views.py
+++ azu_bot_django/reservation/views.py
@@ -1,40 +1,216 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets
-from rest_framework.response import Response
-
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_protect
+from .forms import BookingForm, LocationForm, ComboForm, DishesForm
+from .models import Reservation, OrderSets
 from cafe.models import Cafe
-from reservation.models import Reservation
-from reservation.serializers import (ReservationReadSerializer,
-                                     ReservationWriteSerializer)
-from reservation.validation import cancell_reservation
+import json
 
 
-class ReservationViewSet(viewsets.ModelViewSet):
-    queryset = Reservation.objects.all()
+@csrf_protect
+def index(request):
+    response_data = {
+        'app_name': 'AzuBot',
+    }
+    return JsonResponse(response_data)
 
-    def get_cafe(self):
-        cafe_id = self.kwargs.get('cafe_id')
-        return get_object_or_404(Cafe, id=cafe_id)
 
-    def get_queryset(self):
-        return Reservation.objects.filter(cafe=self.get_cafe())
+@csrf_protect
+def book_table(request):
+    if request.method == 'POST':
+        booking_form = BookingForm(request.POST)
+        if booking_form.is_valid():
+            date = booking_form.cleaned_data.get('date')
+            cafe = booking_form.cleaned_data.get('cafe')
+            name = booking_form.cleaned_data.get('name')
+            number = booking_form.cleaned_data.get('number')
+            set_instance = booking_form.cleaned_data.get('set')
+            quantity = booking_form.cleaned_data.get('quantity')
+            if set_instance and set_instance.price >= 0 and quantity > 0:
+                booking = Reservation.objects.create(
+                    date=date,
+                    cafe=cafe,
+                    name=name,
+                    number=number,
+                    status='booked')
+                order_sets = OrderSets(
+                    reservation=booking,
+                    sets=set_instance,
+                    quantity=quantity)
+                order_sets.save()
+                response_data = {
+                    'status': 'success',
+                    'message': booking.id,
+                }
+            else:
+                response_data = {
+                    'status': 'error',
+                    'message': 'Ошибка меню или его количества.',
+                }
+        else:
+            response_data = {
+                'status': 'error',
+                'message': 'Ошибка данных.',
+            }
+    else:
+        response_data = {
+            'status': 'error',
+            'message': 'Неверный метод запроса.',
+        }
+    return JsonResponse(response_data)
 
-    def get_serializer_class(self):
-        method = self.request.method
-        if method in ('PATCH', 'GET'):
-            return ReservationReadSerializer
-        return ReservationWriteSerializer
 
-    def perform_create(self, serializer):
-        cafe = self.get_cafe()
-        return serializer.save(cafe=cafe)
+@csrf_protect
+def create_location(request):
+    if request.method == 'POST':
+        form = LocationForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            address = form.cleaned_data['address']
+            location = Cafe(name=name, address=address)
+            location.save()
+            response_data = {
+                'status': 'success',
+                'message': 'Создано',
+            }
+        else:
+            response_data = {
+                'status': 'error',
+                'message': 'Ошибка данных.',
+            }
+    else:
+        response_data = {
+            'status': 'error',
+            'message': 'Неверный метод запроса.',
+        }
+    return JsonResponse(response_data)
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        instance = self.perform_create(serializer)
-        instance_serializer = ReservationReadSerializer(instance)
-        return Response(instance_serializer.data)
 
-    def perform_update(self, serializer):
-        cancell_reservation(serializer)
+@csrf_protect
+def update_location(request, location_id):
+    location = get_object_or_404(Cafe, id=location_id)
+    if request.method == 'POST':
+        form = LocationForm(request.POST, instance=location)
+        if form.is_valid():
+            form.save()
+            response_data = {
+                'status': 'success',
+                'message': 'Адрес успешно обновлен',
+            }
+        else:
+            response_data = {
+                'status': 'error',
+                'message': 'Ошибка данных.',
+            }
+    else:
+        response_data = {
+            'status': 'error',
+            'message': 'Неверный метод запроса.',
+        }
+    return JsonResponse(response_data)
+
+
+@csrf_protect
+def delete_location(request, location_id):
+    location = get_object_or_404(Cafe, id=location_id)
+    if request.method == 'POST':
+        if location:
+            location.delete()
+            response_data = {
+                'status': 'success',
+                'message': 'Адрес успешно удален',
+            }
+        else:
+            response_data = {
+                'status': 'error',
+                'message': 'Адрес не найден.',
+            }
+    else:
+        response_data = {
+            'status': 'error',
+            'message': 'Неверный метод запроса.',
+        }
+    return JsonResponse(response_data)
+
+
+@csrf_protect
+def update_booking(request, booking_id):
+    if request.method == 'PUT':
+        try:
+            data = json.loads(request.body)
+            payment_status = data.get('payment_status')
+            if payment_status is not None:
+                booking = Reservation.objects.get(pk=booking_id)
+                booking.payment_status = payment_status
+                booking.save()
+                response_data = {
+                    'status': 'success',
+                    'message': 'Бронирование успешно обновлено.',
+                }
+            else:
+                response_data = {
+                    'status': 'error',
+                    'message': 'Требуется оплата.',
+                }
+        except json.JSONDecodeError:
+            response_data = {
+                'status': 'error',
+                'message': 'Ошибка JSON.',
+            }
+        except Reservation.DoesNotExist:
+            response_data = {
+                'status': 'error',
+                'message': 'Бронирование не найдено.',
+            }
+    else:
+        response_data = {
+            'status': 'error',
+            'message': 'Неверный метод запроса.',
+        }
+    return JsonResponse(response_data)
+
+
+@csrf_protect
+def create_combo(request):
+    if request.method == 'POST':
+        form = ComboForm(request.POST)
+        if form.is_valid():
+            form.save()
+            response_data = {
+                'status': 'success',
+                'message': 'Комбо-сет создан успешно',
+            }
+        else:
+            response_data = {
+                'status': 'error',
+                'errors': form.errors,
+            }
+    else:
+        response_data = {
+            'status': 'error',
+            'message': 'Неверный метод запроса.',
+        }
+    return JsonResponse(response_data)
+
+
+@csrf_protect
+def create_dish(request):
+    if request.method == 'POST':
+        form = DishesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            response_data = {
+                'status': 'success',
+                'message': 'Блюдо успешно создано',
+            }
+        else:
+            response_data = {
+                'status': 'error',
+                'message': 'Ошибка данных.',
+            }
+    else:
+        response_data = {
+            'status': 'error',
+            'message': 'Неверный метод запроса.',
+        }
+    return JsonResponse(response_data)
